713646d7e667da5ed9167a5dae28baffcb3ce6b8
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-02 10:29:55 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,40 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
+    def __init__(self, title="River and the Source", page_count=69):
+        self._title = title
+        self._page_count = page_count
+        self._turn_page = 0  
         
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter   
+    def title(self, title):
+        self._title = title
+        
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter   
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+            
+     
+    def turn_page(self):
+        self._turn_page = 1
+        print("Flipping the page...wow, you read fast!")
+    
+book = Book(title="River and the Source", page_count=69)
+
+book.title = "The Rational Male"
+book.page_count = 1000
+
+print(book.title)
+print(book.page_count)
+
+
+book.turn_page = 3
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    condition="New"
+    def __init__(self, brand, size):
+        self._size=size
+        self._brand=brand
+        self.condition="Used"
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,size):
+        if isinstance(size,int):
+            self._size=size
+        else:
+            print("size must be an integer")
+    
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self,brand):
+        self._brand=brand
+        
+    def cobble(self):
+        self.condition="New"
+        print("Your shoe is as good as new!")
+        
